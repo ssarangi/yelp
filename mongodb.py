1b40fc9b1824a58328caf6ab202959c4997d84b1
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2015-08-14 15:18:30 -0700

    Put the collection class back to allow for more complicated queries

--- mongodb.py
+++ mongodb.py
@@ -3,10 +3,41 @@ __author__ = 'sarangis'
 import pandas as pd
 from pymongo import MongoClient
 
+class Collection:
+    def __init__(self, db_obj, name):
+        self.name = name
+        self.db = db_obj
+
+    def _query(self, query_dict={}):
+        cursor = None
+        if bool(query_dict):
+            cursor = self.db(self.name).find()
+        else:
+            cursor = self.db[self.name].find(query_dict)
+
+        return cursor
+
+    def to_dataframe(self, query_dict={}, no_id=True):
+        cursor = self._query(query_dict)
+
+        # Expand the cursor and construct the dataframe
+        df = pd.DataFrame(list(cursor))
+
+        if (no_id):
+            del df['_id']
+
+        return df
+
 class MongoDBHelper:
     def __init__(self, db, host='localhost', port=27017, username=None, password=None, no_id=True):
         # Connect to MongoDB
         self.db = self._connect_mongo(host=host, port=port, username=username, password=password, db=db)
+
+        # initialize all the members with collection classes
+        collection_list = self.db.collection_names(include_system_collections=False)
+
+        for collection in collection_list:
+            setattr(self, collection, Collection(self.db, collection))
 
     def _connect_mongo(self, host, port, username, password, db):
         """ A util for making a connection to mongo """
