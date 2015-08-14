e4e7f4dcc68120888c83b51af9d48a11a4698a0d
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2015-08-14 11:57:55 -0700

    first commit for yelp dataset loading

---
+++ create_db.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\MongoDB\Server\3.0\bin\mongoimport" --db yelp --collection businesses yelp_academic_dataset_business.json
+"C:\Program Files\MongoDB\Server\3.0\bin\mongoimport" --db yelp --collection checkins yelp_academic_dataset_checkin.json
+"C:\Program Files\MongoDB\Server\3.0\bin\mongoimport" --db yelp --collection reviews yelp_academic_dataset_review.json
+"C:\Program Files\MongoDB\Server\3.0\bin\mongoimport" --db yelp --collection tips yelp_academic_dataset_tip.json
+"C:\Program Files\MongoDB\Server\3.0\bin\mongoimport" --db yelp --collection users yelp_academic_dataset_user.json
---
+++ mongodb.py
@@ -0,0 +1,49 @@
+__author__ = 'sarangis'
+
+import pandas as pd
+from pymongo import MongoClient
+
+
+def _connect_mongo(host, port, username, password, db):
+    """ A util for making a connection to mongo """
+
+    if username and password:
+        mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+        conn = MongoClient(mongo_uri)
+    else:
+        conn = MongoClient(host, port)
+
+
+    return conn[db]
+
+
+def read_mongo(db, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
+    """ Read from Mongo and Store into DataFrame """
+
+    # Connect to MongoDB
+    db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
+
+    # Make a query to the specific DB and Collection
+    cursor = db[collection].find(query)
+
+    # Expand the cursor and construct the DataFrame
+    df =  pd.DataFrame(list(cursor))
+
+    # Delete the _id
+    if no_id:
+        del df['_id']
+
+    return df
+
+businesses = read_mongo('yelp', 'businesses')
+checkins = read_mongo('yelp', 'checkins')
+reviews = read_mongo('yelp', 'reviews')
+tips = read_mongo('yelp', 'tips')
+users = read_mongo('yelp', 'users')
+
+print(businesses)
+print(checkins)
+print(reviews)
+print(tips)
+print(users)
+
