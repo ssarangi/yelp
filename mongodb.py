52ae4864405af693eebe68efc73d7f43dcf6a824
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2015-08-14 14:19:12 -0700

    Made the helper robust and easier to work with

--- mongodb.py
+++ mongodb.py
@@ -3,47 +3,56 @@ __author__ = 'sarangis'
 import pandas as pd
 from pymongo import MongoClient
 
+class Collection:
+    def __init__(self, db_obj, name):
+        self.name = name
+        self.db = db_obj
 
-def _connect_mongo(host, port, username, password, db):
-    """ A util for making a connection to mongo """
+    def _query(self, query_dict={}):
+        cursor = None
+        if bool(query_dict):
+            cursor = self.db(self.name).find()
+        else:
+            cursor = self.db[self.name].find(query_dict)
 
-    if username and password:
-        mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
-        conn = MongoClient(mongo_uri)
-    else:
-        conn = MongoClient(host, port)
+        return cursor
+
+    def to_dataframe(self, query_dict={}, no_id=True):
+        cursor = self._query(query_dict)
+
+        # Expand the cursor and construct the dataframe
+        df = pd.DataFrame(list(cursor))
+
+        if (no_id):
+            del df['_id']
+
+        return df
+
+class MongoDBHelper:
+    def __init__(self, db, host='localhost', port=27017, username=None, password=None, no_id=True):
+        # Connect to MongoDB
+        self.db = self._connect_mongo(host=host, port=port, username=username, password=password, db=db)
+
+        # initialize all the members with collection classes
+        collection_list = self.db.collection_names(include_system_collections=False)
+
+        for collection in collection_list:
+            setattr(self, collection, Collection(self.db, collection))
+
+    def _connect_mongo(self, host, port, username, password, db):
+        """ A util for making a connection to mongo """
+        if username and password:
+            mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+            conn = MongoClient(mongo_uri)
+        else:
+            conn = MongoClient(host, port)
+
+        return conn[db]
+
+    def collections(self):
+        return self.db.collection_names(include_system_collections=False)
 
 
-    return conn[db]
-
-
-def read_mongo(db, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
-    """ Read from Mongo and Store into DataFrame """
-
-    # Connect to MongoDB
-    db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
-
-    # Make a query to the specific DB and Collection
-    cursor = db[collection].find(query)
-
-    # Expand the cursor and construct the DataFrame
-    df =  pd.DataFrame(list(cursor))
-
-    # Delete the _id
-    if no_id:
-        del df['_id']
-
-    return df
-
-businesses = read_mongo('yelp', 'businesses')
-checkins = read_mongo('yelp', 'checkins')
-reviews = read_mongo('yelp', 'reviews')
-tips = read_mongo('yelp', 'tips')
-users = read_mongo('yelp', 'users')
-
-print(businesses)
-print(checkins)
-print(reviews)
-print(tips)
-print(users)
-
+if __name__ == "__main__":
+    mongo_helper = MongoDBHelper('yelp')
+    print(mongo_helper.businesses.to_dataframe())
